8c9bce874d2e94e5abac5a7a5b78b135a9875ea5
Ayu <ayuamperabp@gmail.com>
2024-09-20 16:46:31 +1200

    Added comments

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <driver/gpio.h>
-
 #include <esp_system.h>
 #include <esp_wifi.h>
 #include <freertos/FreeRTOS.h>
@@ -8,13 +7,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
 #include <math.h>
 #include <esp_log.h>
 #include <esp_sntp.h>
 #include <nvs_flash.h>
-
 #include "fonts.h"
 #include "graphics.h"
 #include "input_output.h"
@@ -47,28 +43,33 @@ void app_main() {
     // if you are anywhere else in the world, this will need to be changed
     setenv("TZ", "NZST-12:00:00NZDT-13:00:00,M9.5.0,M4.1.0", 0);
     tzset();
-    // initialise graphics and lcd display
+
+    // Initialize graphics, clear screen and set orientation to always portrait
     graphics_init();
     cls(0);
-
     set_orientation(PORTRAIT);
 
-    // main menu
+    // Set current screen to Menu which is the first screen to be displayed
     enum CurrentScreen currentScreen = MENU;
     
     int last_score = 0;
-
-    int sel=0;
+    int selected_menu = 0;
     
+    // Menu has two entries: to start the game and to view the highscore
     char *entries[]={"Start Game", "Highscore"};
 
     while(1) {
 
         switch (currentScreen){
-
+            
+            // Show the Main Menu screen / Welcome screen 
             case MENU:
-                sel=render_menu("Welcome",sizeof(entries)/sizeof(char *),entries,sel);
-                switch(sel) {
+
+                // Show the menu and wait for the respond based on the selection
+                selected_menu = render_menu("Welcome",sizeof(entries)/sizeof(char *), entries, selected_menu);
+
+                switch (selected_menu) {
+
                     case 0:
                         currentScreen = GAME;          
                         break;
@@ -81,16 +82,19 @@ void app_main() {
 
                 break;
 
+            // Show the Game screen to play the game
             case GAME:
                 last_score = render_game();
                 currentScreen = SCORE;
                 break;
 
+            // Show the Score screen to display the score after the game is over
             case SCORE:
                 render_score(last_score);
                 currentScreen = MENU;
                 break;
 
+            // Show the High Score screen to display the high score from the game
             case HIGHSCORE:
                 // render the highscore
                 break;
